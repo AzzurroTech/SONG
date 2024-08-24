857e7266b3c2d7d4fe884fd1c09cc8a8e77b0cab
Matthew Salvatore Giancola <matthew.giancola42@gmail.com>
2024-08-24 13:25:39 -0400

    core server for SONG

--- server.go
+++ server.go
@@ -1,32 +1,20 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
-	"time"
 
-	veni "github.com/Emperor42/veni"
-	vidi "github.com/Emperor42/vidi"
-	_ "github.com/go-sql-driver/mysql"
+	veni "../veni"
+	vidi "../vidi"
 )
 
 func main() {
-	// Opening a driver typically will not attempt to connect to the database.
-	db, err := sql.Open("mysql", "user:password@/dbname")
-	if err != nil {
-		panic(err)
+	http.HandleFunc("/call", veni.Handler)
+	http.HandleFunc("/data", vidi.Handler)
+
+	fmt.Printf("Starting server at port 3000\n")
+	if err := http.ListenAndServe(":3000", nil); err != nil {
+		log.Fatal(err)
 	}
-	// See "Important settings" section.
-	db.SetConnMaxLifetime(time.Minute * 3)
-	db.SetMaxOpenConns(10)
-	db.SetMaxIdleConns(10)
-	fmt.Println("DB has been setup")
-	//root server handling
-	http.Handle("/", veni.Load(http.Dir("./")))
-	//Access Vidi database
-	http.Handle("/base/", vidi.Load(db))
-	fmt.Println("Listen on port 3000")
-	//serve the server
-	http.ListenAndServe(":3000", nil)
 }
