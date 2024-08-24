5c695652bfc8772072da87fb10654ffa28887813
Matthew Salvatore Giancola <matthew.giancola42@gmail.com>
2024-08-24 15:10:25 -0400

    cleanup of SONG

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/AzzurroTech/SONG
 
 go 1.23.0
+
+replace github.com/Emperor42/vidi => ../vidi
+
+replace github.com/Emperor42/veni => ../veni
+
+require (
+	github.com/Emperor42/veni v0.0.0-00010101000000-000000000000
+	github.com/Emperor42/vidi v0.0.0-00010101000000-000000000000
+)
--- main.go
+++ main.go
@@ -5,13 +5,13 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/Emperor42/veni"
-	"github.com/Emperor42/vidi"
+	veni "github.com/Emperor42/veni"
+	vidi "github.com/Emperor42/vidi"
 )
 
 func main() {
-	http.Handle("/call", &veni.VeniContext{"veni"})
-	http.Handle("/data", &vidi.VidiContext{"veni"})
+	http.Handle("/call", &veni.VeniContext{Name: "veni"})
+	http.Handle("/data", &vidi.VidiContext{Name: "vidi"})
 
 	fmt.Printf("Starting server at port 3000\n")
 	if err := http.ListenAndServe(":3000", nil); err != nil {
