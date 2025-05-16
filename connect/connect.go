b64e18ebbb9ef5ed87a1caf2c2f7fba14e7dbe69
Matthew Salvatore Giancola <matthew.giancola42@gmail.com>
2025-05-16 12:05:19 -0400

    SONG circular deps fixed

--- connect/connect.go
+++ connect/connect.go
@@ -1,15 +1,34 @@
 package connect
 
 import (
-	song "github.com/AzzurroTech/SONG"
+	"net/http"
 )
 
-type ConnectAPI struct {
-	song.API
+type API struct {
+	routeMap map[string](func(http.ResponseWriter, *http.Request) http.ResponseWriter)
 }
 
-func InitAPI() *ConnectAPI {
-	a := song.InitAPI()
-	//make changes to a as required
-	return a
+func InitAPI() *API {
+	var a API
+	a.routeMap = make(map[string](func(http.ResponseWriter, *http.Request) http.ResponseWriter))
+	return &a
+}
+
+func (a *API) AddRoute(routeName string, handle func(http.ResponseWriter, *http.Request) http.ResponseWriter) {
+	a.routeMap[routeName] = handle
+}
+
+func (a *API) RemoveRoute(routeName string) {
+	delete(a.routeMap, routeName)
+}
+
+func (a *API) Process(w http.ResponseWriter, r *http.Request) http.ResponseWriter {
+	path := r.URL.Path
+	routeFunc, hasRoute := a.routeMap[path]
+	if hasRoute {
+		w = routeFunc(w, r)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
+	return w
 }
--- song.go
+++ song.go
@@ -5,46 +5,9 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
-
-	"github.com/AzzurroTech/SONG/connect"
 	//veni "github.com/Emperor42/veni"
 	//vidi "github.com/Emperor42/vidi"
 )
-
-func test() {
-	test := connect.InitAPI()
-	test.AddRoute("test", nil)
-	test.RemoveRoute("test")
-}
-
-type API struct {
-	routeMap map[string](func(http.ResponseWriter, *http.Request) http.ResponseWriter)
-}
-
-func InitAPI() *API {
-	var a API
-	a.routeMap = make(map[string](func(http.ResponseWriter, *http.Request) http.ResponseWriter))
-	return &a
-}
-
-func (a *API) AddRoute(routeName string, handle func(http.ResponseWriter, *http.Request) http.ResponseWriter) {
-	a.routeMap[routeName] = handle
-}
-
-func (a *API) RemoveRoute(routeName string) {
-	delete(a.routeMap, routeName)
-}
-
-func (a *API) Process(w http.ResponseWriter, r *http.Request) http.ResponseWriter {
-	path := r.URL.Path
-	routeFunc, hasRoute := a.routeMap[path]
-	if hasRoute {
-		w = routeFunc(w, r)
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-	}
-	return w
-}
 
 func Serve() {
 	/*
